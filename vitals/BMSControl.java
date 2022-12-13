3f2d35b422b3bf043e520101244102aea64dc1c1
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-12-13 11:20:41 +0530

    Create BMSControl.java

---
+++ vitals/BMSControl.java
@@ -0,0 +1,31 @@
+package vitals;
+public class EBMS{
+  
+  public boolean isBatteryParameterWithinLimits(float parameter,float minLimit, float maxLimit)
+  {
+    boolean result = true;
+    if(parameter >= minLimit && parameter <= maxLimit) {
+            result=false;
+        }
+    return result;
+  }
+  
+  public boolean isTemperatureinValidRange(float temperature)
+  {
+    return (isBatteryParameterWithinLimits(temperature,BMSConstants.MinTemperature,BMSConstants.MaxTemperature));
+  }
+  
+   public boolean isSOCinValidRange(float soc)
+  {
+    return (isBatteryParameterWithinLimits(soc,BMSConstants.MinSoc,BMSConstants.MaxSoc));
+  }
+         
+  public boolean isChargeRateinValidRange(float chargeRate)
+   {
+      return (chargeRate >= BMSConstants.MaxChargeRate);
+   }
+            
+   public boolean batteryIsOk(float temperature, float soc, float chargeRate) {
+     return (isTemperatureinValidRange(temperature) && isSOCinValidRange(soc) && isChargeRateinValidRange(chargeRate));
+   }
+            
