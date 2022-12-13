b7047088cd1f4977767eb399bcf8ab5e68fa9af2
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-12-13 11:15:24 +0530

    Create BMSConstants.java

---
+++ vitals/BMSConstants.java
@@ -0,0 +1,11 @@
+package vitals;
+
+public class BMSConstants {
+
+public static final float MaxTemperature = 45f;
+public static final float MinTemperature = 0f;
+public static final float MaxSoc = 80f;
+public static final float MinSoc = 20f;
+public static final float MaxChargeRate = 0.8f;
+
+}
